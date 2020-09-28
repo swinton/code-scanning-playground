6c8b105a08a502558e9938fc2d3d2fef4784f984
Steve Winton <stevewinton@gmail.com>
2020-09-28 14:01:45 -0500

    Implement check run creation

--- .github/actions/sarif-to-checks/index.js
+++ .github/actions/sarif-to-checks/index.js
@@ -2,6 +2,15 @@ const path = require('path');
 const core = require('@actions/core');
 const github = require('@actions/github').getOctokit(process.env.GITHUB_TOKEN);
 
+annotationLevelFromResultLevel = (resultLevel) => {
+  const resultLevelMap = {
+    'error': 'failure',
+    'warning': 'warning',
+    'note': 'notice',
+  }
+  return resultLevelMap[resultLevel];
+};
+
 async function main () {
   try {
     // Load SARIF
@@ -10,11 +19,41 @@ async function main () {
     core.info(`sarifPath: ${ sarifPath }`);
 
     // Process SARIF
-    const results = data.runs[0].results;
-    await Promise.all(results.map(async result => {
-      console.log(JSON.stringify(result, null, 4));
-      return result;
+    const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/');
+    const name = data.runs[0].tool.driver.name;
+
+    const annotations = await Promise.all(results.map(async result => {
+      const location = result.locations[0].physicalLocation;
+
+      return {
+        path: location.artifactLocation.uri,
+        start_line: location.region.startLine,
+        end_line: location.region.endLine || location.region.startLine,
+        start_column: location.region.startColumn,
+        end_column: location.region.endColumn || location.region.startColumn,
+        annotation_level: annotationLevelFromResultLevel(result.level),  // Can be one of notice, warning, or failure
+        message: result.message.text,
+        title: result.ruleId,
+      }
     }));
+
+    // https://docs.github.com/en/free-pro-team@latest/rest/reference/checks
+    const response = await github.request("POST /repos/:owner/:repo/check-runs", {
+      mediaType: {
+        previews: ['antiope']
+      },
+      owner,
+      repo,
+      name,
+      head_sha: process.env.GITHUB_SHA,
+      status: 'completed',
+      conclusion: results.length === 0 ? 'success' : 'failure',
+      output: {
+        annotations
+      }
+    });
+    core.info(`Response status: ${ response.status }.`);
+
   } catch (e) {
     core.setFailed(e);
   }
